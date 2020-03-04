27805bdded1be26a4852b36db2fb402099a61889
Notatea <Notatea@users.noreply.github.com>
2020-03-04 19:59:09 +0200

    Shooting 2.0 !!!!

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float DestroyDelay = 0.1f;
+    public float Damage = 1.0f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHPController>().HP -= Damage;
+            collision.gameObject.GetComponent<EnemyHPController>().GetsDamage = true;
+
+            DestroyProperly();
+        }
+    }
+
+    void DestroyProperly()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        Destroy(gameObject, DestroyDelay);
+    }
+}
+
---
+++ Assets/Scripts/EnemyHPController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHPController : MonoBehaviour
+{
+    public float HP = 1.0f;
+    public bool GetsDamage = false;
+    public Color DamageColor;
+    public float BlinkDuration = 1.0f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (GetsDamage == true)
+        {
+
+            Blink();
+           
+            GetsDamage = false;
+        }
+
+        if(rend.material.color != Color.black)
+        {
+            
+            Debug.Log("ColorChange2");
+        }
+
+    }
+
+    private void Update()
+    {
+        rend.material.color = Color.Lerp(rend.material.color, Color.black, 1 / BlinkDuration);
+    }
+
+
+    void Blink()
+    {
+        rend.material.color = DamageColor;
+        Debug.Log("ColorChange1");    
+    }
+
+}
+
+
